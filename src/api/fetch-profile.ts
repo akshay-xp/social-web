b0f21780606642e040036e321bf78542c952e362
Akshay PS <psakshay2022@gmail.com>
2024-04-14 16:04:21 +0530

    add user follow

--- src/api/fetch-profile.ts
+++ src/api/fetch-profile.ts
@@ -1,14 +1,21 @@
 import { api } from "./axios";
 
+type Following = {
+  followerId: string;
+};
+
 type User = {
   id: string;
   email: string;
   username: string;
   fullname: string;
   bio: string;
+  following: Following[];
 };
 
-export const fetchProfile = async (userId: string) => {
-  const response = await api.get<User>(`/users/${userId}`);
+export const fetchProfile = async (userId: string, followerId?: string) => {
+  const response = await api.get<User>(`/users/${userId}`, {
+    params: { followerId },
+  });
   return response.data;
 };
--- src/routes/_base/profile/$userId.tsx
+++ src/routes/_base/profile/$userId.tsx
@@ -5,28 +5,38 @@ import { PostCard } from "@/components/blocks/post-card";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Toggle } from "@/components/ui/toggle";
+import { useAuth } from "@/hooks/use-auth";
+import { usePrivateApi } from "@/hooks/use-private-api";
 import {
   queryOptions,
+  useMutation,
   useQuery,
+  useQueryClient,
   useSuspenseQuery,
 } from "@tanstack/react-query";
 import { Link, createFileRoute } from "@tanstack/react-router";
 
-const profileQueryOptions = (userId: string) =>
+const profileQueryOptions = (userId: string, followerId: string) =>
   queryOptions({
-    queryKey: ["profile", { userId }],
-    queryFn: () => fetchProfile(userId),
+    queryKey: ["profile", { userId, followerId }],
+    queryFn: () => fetchProfile(userId, followerId),
   });
 
 export const Route = createFileRoute("/_base/profile/$userId")({
   component: ProfilePage,
+  loader: ({ context: { queryClient, auth }, params: { userId } }) =>
+    queryClient.ensureQueryData(profileQueryOptions(userId, auth.auth.userId)),
   pendingComponent: () => <div>Loading...</div>,
 });
 
 function ProfilePage() {
+  const { userId: currentUserId } = useAuth().auth;
   const userId = Route.useParams().userId;
-  const profileQuery = useSuspenseQuery(profileQueryOptions(userId));
+  const profileQuery = useSuspenseQuery(
+    profileQueryOptions(userId, currentUserId),
+  );
   const profile = profileQuery.data;
+  const isFollowing = profile.following.length === 1;
   const postsQuery = useQuery({
     queryKey: ["posts", { userId }],
     queryFn: () => fetchPosts(userId),
@@ -39,6 +49,30 @@ function ProfilePage() {
     enabled: !!userId,
   });
   const comments = commentQuery.data;
+  const api = usePrivateApi();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (isFollowing: boolean) => {
+      if (isFollowing) {
+        return api.post(`/follow/${userId}`, {
+          withCredentials: true,
+        });
+      } else {
+        return api.delete(`/follow/${userId}`, {
+          withCredentials: true,
+        });
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["profile", { userId, followerId: currentUserId }],
+      });
+    },
+  });
+
+  const handleFollow = () => {
+    mutation.mutate(!isFollowing);
+  };
 
   return (
     <div className="gap-6 flex flex-col p-4">
@@ -49,8 +83,18 @@ function ProfilePage() {
         </div>
         <p>{profile.bio}</p>
         <div className="flex gap-1">
-          <Toggle variant={"outline"}>Follow</Toggle>
-          <Button variant={"outline"}>Edit</Button>
+          {currentUserId && currentUserId !== userId && (
+            <Toggle
+              variant={"outline"}
+              pressed={isFollowing}
+              onClick={handleFollow}
+            >
+              {isFollowing ? "Following" : "Follow"}
+            </Toggle>
+          )}
+          {currentUserId && currentUserId === userId && (
+            <Button variant={"outline"}>Edit</Button>
+          )}
         </div>
       </div>
       <Tabs defaultValue="posts">
